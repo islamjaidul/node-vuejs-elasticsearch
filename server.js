27523ba7b3531e9dfbc5254517912e0580499889
islamjaidul <jaidul26@gmail.com>
2018-08-20 01:41:02 +0000

    server.js changed

--- server.js
+++ server.js
@@ -16,8 +16,10 @@ app.set('view engine', 'hbs');
 var {mongoose} = require('./config/database');
 var {User} = require('./app/Models/User');
 
-console.log("Running in port 3000");
-app.listen(3000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
 
 app.get('/', (req, res) => {
 	res.json({
